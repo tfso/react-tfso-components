48f33b1215e482a48949f558f3903aca6ddc86b8
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-03-07 14:39:52 +0100

    Use theme. Dont allow old react

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import './index.css'
-import App from './App'
+import {theme} from 'react-tfso-components'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {ThemeProvider as StyledThemeProvider} from 'styled-components'
+import {MuiThemeProvider} from '@material-ui/core/styles'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './demos/App'
+
+ReactDOM.render(
+    <React.Fragment>
+        <CssBaseline />
+        <MuiThemeProvider theme={theme.materialuiTheme}>
+            <StyledThemeProvider theme={theme.styledTheme}>
+                <App />
+            </StyledThemeProvider>
+        </MuiThemeProvider>
+    </React.Fragment>,
+    document.getElementById('root')
+)
