1755553748c826036439624e5f4964ef8b578e02
sondresj <sondre.skaardal.jensen@gmail.com>
2019-03-26 15:01:57 +0100

    Add GitHub link from Demo

--- src/demo/Layout.tsx
+++ src/demo/Layout.tsx
@@ -10,12 +10,21 @@ import {layout} from '../lib'
 import Misc from './demos/Misc'
 import Utils from './demos/Utils'
 import Icons from './demos/Icons'
+import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon'
+import Link from '@material-ui/core/Link'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const demoComponents = {
     Misc: Misc,
     Utils: Utils,
     Icons: Icons,
 }
+
+const GitHubIcon = (props: SvgIconProps) => (
+    <SvgIcon {...props}>
+        <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" />
+    </SvgIcon>
+)
 
 export default class Layout extends Component<any, any>{
     state = {
@@ -59,7 +68,11 @@ export default class Layout extends Component<any, any>{
         )
         const topMenu = (
             <layout.TopMenu>
-                Top menu
+                <Link href='https://github.com/tfso/react-tfso-components'>
+                    <Tooltip title='View on GitHub'>
+                        <GitHubIcon fontSize='large'/>
+                    </Tooltip>
+                </Link>
             </layout.TopMenu>
         )
 
