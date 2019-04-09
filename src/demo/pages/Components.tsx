c51c71a8d8bcd540a8bf716eb1a82ade051e9801
Jeanette Stavsholt Gusjås <jeanette.stavsholt@live.no>
2019-04-09 09:55:25 +0200

    Starting to build profile-card cmp

--- src/demo/pages/Components.tsx
+++ src/demo/pages/Components.tsx
@@ -7,9 +7,11 @@ import SearchFieldDemo from '../demos/SearchFieldDemo'
 import TextFieldEditorDemo from '../demos/TextFieldEditorDemo'
 import ConfirmationDialogDemo from '../demos/ConfirmationDialogDemo'
 import EmojiDemo from '../demos/EmojiDemo'
+import ProfileCardDemo from '../demos/ProfileCardDemo'
 // import DashBoardDemo from '../demos/DashBoardDemo'
 
 export default () => (<>
+    <ProfileCardDemo />
     <EmojiDemo />
     <AlertDemo />
     <BigNumberDemo />
