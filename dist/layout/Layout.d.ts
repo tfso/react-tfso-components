b3353f61f622832041dfbe1b586411832a96b124
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-04-12 14:48:40 +0200

    Published by publish-to-git ca424dd309303c6eeedf8b4221c4e8ce8d116ed2 (HEAD -> refs/heads/master, refs/remotes/origin/master) Bump v

---
+++ dist/layout/Layout.d.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ScreenSizeData } from '../ScreenSize';
+export declare const LayoutWrapper: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const LayoutHeader: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const LayoutBody: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const LayoutBodyLeft: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const LayoutBodyRight: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+declare type LayoutProps = {
+    title: string;
+    docTitle: string;
+    menuContent?: React.ReactNode;
+    topMenuContent?: React.ReactNode;
+} & {
+    screenSize: ScreenSizeData;
+};
+declare const _default: React.ComponentType<Pick<LayoutProps, "title" | "docTitle" | "menuContent" | "topMenuContent">>;
+export default _default;
+//# sourceMappingURL=Layout.d.ts.map
---
+++ dist/theme/index.d.ts
@@ -0,0 +1,63 @@
+import 'styled-components';
+import './index.css';
+export declare const materialuiTheme: import("@material-ui/core/styles").Theme;
+export declare const styledTheme: {
+    tfso: {
+        colors: {
+            tfso: string;
+            baseColor: string;
+            baseLightColor: string;
+            baseLight2Color: string;
+            baseLight3Color: string;
+            baseLight4Color: string;
+            baseLight5Color: string;
+            blueColor: string;
+            blueDarkColor: string;
+            blueDesaturatedColor: string;
+            grayBlueColor: string;
+            tealColor: string;
+            greenColor: string;
+            greenLightColor: string;
+            purpleColor: string;
+            redColor: string;
+            redLightColor: string;
+            orangeColor: string;
+            yellowColor: string;
+            blackColor: string;
+            grayColor: string;
+            grayDarkColor: string;
+            grayDarkerColor: string;
+            grayLightColor: string;
+            grayLighterColor: string;
+            grayLightestColor: string;
+            whiteColor: string;
+            whiteDarkColor: string;
+            whiteDarkerColor: string;
+            bodyBackgroundColor: string;
+            listOverBackgroundColor: string;
+            borderColor: string;
+            tabBarColor: string;
+        };
+        palette: {
+            primary: string;
+            primaryLight: string;
+            success: string;
+            warning: string;
+            alert: string;
+            delete: string;
+        };
+        layout: {
+            containerMargin: string;
+        };
+    };
+    mui: import("@material-ui/core/styles").Theme;
+};
+export declare type TfsoTheme = typeof styledTheme;
+declare module 'styled-components' {
+    interface DefaultTheme extends TfsoTheme {
+    }
+    type WithTheme = {
+        theme: DefaultTheme;
+    };
+}
+//# sourceMappingURL=index.d.ts.map
