08ab87952f343dbafbf4ddf461db1772567c5ea2
Magnus Tovslid <magnus.tovslid@gmail.com>
2019-04-12 14:39:33 +0200

    Updates

---
+++ precache-manifest.1e1bc6de34a4283d72e1e19ccc6dc9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c5a8edee8ab9a7980cc",
+    "url": "/react-tfso-components/static/js/runtime~main.9fcd8f8c.js"
+  },
+  {
+    "revision": "5b8870e95bcb9af6036e",
+    "url": "/react-tfso-components/static/js/main.dc7fbe4a.chunk.js"
+  },
+  {
+    "revision": "fabe6a60250c2dd9daf0",
+    "url": "/react-tfso-components/static/js/2.283b56d8.chunk.js"
+  },
+  {
+    "revision": "5b8870e95bcb9af6036e",
+    "url": "/react-tfso-components/static/css/main.95ebd342.chunk.css"
+  },
+  {
+    "revision": "fabe6a60250c2dd9daf0",
+    "url": "/react-tfso-components/static/css/2.4341f7a5.chunk.css"
+  },
+  {
+    "revision": "0453945f3e8df889358a6eec06f3e7c8",
+    "url": "/react-tfso-components/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tfso-components/precache-manifest.aa8bbbb716a1dd314d5e20c4a555c397.js"
+  "/react-tfso-components/precache-manifest.1e1bc6de34a4283d72e1e19ccc6dc9f8.js"
 );
 
 workbox.clientsClaim();
