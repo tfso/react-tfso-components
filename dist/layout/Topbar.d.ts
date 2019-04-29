59cf75e4812c37abdf1cd02b5c27cb363ac3b3e0
tfsobob <appveyor@24sevenoffice.com>
2019-04-29 18:07:08 +0000

    Published by publish-to-git 7210b344af15079eef22dcccb3a09a44d4cb6d64 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) min-width for profile card

---
+++ dist/layout/Topbar.d.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+export declare type TopBarProps = {
+    onMenuToggle?: () => void;
+    mobile: boolean;
+    children?: React.ReactNode;
+};
+export default class TopBar extends React.PureComponent<TopBarProps> {
+    static propTypes: {
+        onMenuToggle: PropTypes.Requireable<(...args: any[]) => any>;
+        mobile: PropTypes.Validator<boolean>;
+        children: PropTypes.Requireable<PropTypes.ReactNodeLike>;
+    };
+    render(): JSX.Element;
+}
+export declare type TopMenuContentProps = {
+    children: React.ReactNode;
+};
+export declare class TopMenuContent extends React.PureComponent<TopMenuContentProps> {
+    static propTypes: {
+        children: PropTypes.Validator<string | number | boolean | {} | PropTypes.ReactElementLike | PropTypes.ReactNodeArray>;
+    };
+    render(): JSX.Element;
+}
+//# sourceMappingURL=Topbar.d.ts.map
