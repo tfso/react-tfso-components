aa2ee39dd95af4ba55e2596f5929658a7646c96c
tfsobob <appveyor@24sevenoffice.com>
2019-04-29 18:08:18 +0000

    Updates

--- precache-manifest.f167367f86f85233738127951ff6b533.js
+++ precache-manifest.f167367f86f85233738127951ff6b533.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-tfso-components/static/js/runtime~main.9fcd8f8c.js"
   },
   {
-    "revision": "900af1fd233be886d1e8",
-    "url": "/react-tfso-components/static/js/main.8c49b934.chunk.js"
+    "revision": "303756d34ff548d00f5a",
+    "url": "/react-tfso-components/static/js/main.3b4c9ef6.chunk.js"
   },
   {
     "revision": "4427f15fbd098fba6ad6",
     "url": "/react-tfso-components/static/js/2.6255102c.chunk.js"
   },
   {
-    "revision": "900af1fd233be886d1e8",
+    "revision": "303756d34ff548d00f5a",
     "url": "/react-tfso-components/static/css/main.3f4026ee.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-tfso-components/static/css/2.34ed137a.chunk.css"
   },
   {
-    "revision": "3dcb30a9703b545f1a2b7b791fc516fd",
+    "revision": "73a69c9ee2ccf646ce6d83d60b817d30",
     "url": "/react-tfso-components/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tfso-components/precache-manifest.55060513bcc25b64d3f94c1ed02addf2.js"
+  "/react-tfso-components/precache-manifest.f167367f86f85233738127951ff6b533.js"
 );
 
 workbox.clientsClaim();
