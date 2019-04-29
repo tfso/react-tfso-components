1c6cd1a707de79c8eeeab8ea801da54754bb8885
tfsobob <appveyor@24sevenoffice.com>
2019-04-29 10:03:30 +0000

    Published by publish-to-git be268ed49b93efcf1bd8ee2482afadc5eebdf7eb (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Merge branch 'master' of https://github.com/tfso/react-tfso-components

---
+++ src/demo/components/ThemeColor.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import {styledTheme} from '../../lib/theme/index'
+
+const bgColor = (color: '') => {
+    const colors = styledTheme['tfso']['colors']
+    return colors[color]
+}
+
+export type ThemeColorProps = {
+    color: React.ReactNode
+}
+
+const Box = styled.div`
+    background-color: ${({color, theme}) => theme.tfso.colors[color]};
+    width: 230px;
+    height: 230px;
+    text-align: center;
+`
+
+const Txt = styled.div`
+  font-size: 2em;
+  line-height: 2em;
+  color: ${({dark, theme}) => dark ? '#000' : theme.mui.palette.secondary.contrastText}
+`
+
+const ThemeColor = (props: ThemeColorProps) => {
+    const {color} = props
+    return (
+        <Box color={color}>
+            theme.tfso.colors.{color}<br/><br/>
+            <Txt>{bgColor(color)}</Txt>
+            <Txt dark>{bgColor(color)}</Txt>
+        </Box>
+    )
+}
+
+ThemeColor.propTypes = {
+    color: PropTypes.node.isRequired
+}
+
+export default ThemeColor
---
+++ src/lib/layout/Layout.tsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+import TopBar from './Topbar'
+import DocumentTitle from './DocumentTitle'
+import Menu from './Menu'
+import {withScreenSize, ScreenSizeData} from '../ScreenSize'
+
+// Wrap everything
+export const LayoutWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column; // Flex header and body from top to bottom
+`
+
+// Put topbar in here
+export const LayoutHeader = styled.div`
+  
+`
+
+// Put everything below topbar here
+export const LayoutBody = styled.div`
+  flex: 1; // Fill the rest of the page after header has taken its space
+  min-height: 0;
+  display: flex;
+`
+
+// Put menu here
+export const LayoutBodyLeft = styled.div`
+
+`
+
+// Put your app here
+export const LayoutBodyRight = styled.div`
+  flex: 1;
+  min-height: 0;
+  padding: 15px;
+  overflow-y: auto;
+`
+
+type LayoutProps = {
+    docTitle: string
+    menuContent?: React.ReactNode
+    topMenuContent?: React.ReactNode
+} & {
+    screenSize: ScreenSizeData
+}
+
+type LayoutState = {
+    menuOpen: boolean | null
+}
+
+export default withScreenSize(class Layout extends React.PureComponent<LayoutProps, LayoutState>{
+    static propTypes = {
+        docTitle: PropTypes.string.isRequired,
+        menuContent: PropTypes.node,
+        topMenuContent: PropTypes.node
+    }
+
+    state: LayoutState = {
+        menuOpen: null
+    }
+
+    onCloseMenu = () => this.setState({menuOpen: false})
+    onMenuToggle = () => this.setState(state => ({menuOpen: !this.menuIsOpen()}))
+    menuIsOpen(){
+        if(this.state.menuOpen === null){
+            return !this.props.screenSize.mobile
+        }
+        return this.state.menuOpen
+    }
+    onClickContent = () => {
+        if(this.props.screenSize.mobile && this.menuIsOpen()){
+            this.onCloseMenu()
+        }
+    }
+
+    render(){
+        return (
+            <LayoutWrapper>
+                <DocumentTitle text={'24SevenOffice - ' + this.props.docTitle} />
+                <LayoutHeader>
+                    <TopBar
+                        onMenuToggle={this.onMenuToggle}
+                        mobile={this.props.screenSize.mobile}
+                    >
+                        {this.props.topMenuContent}
+                    </TopBar>
+                </LayoutHeader>
+                <LayoutBody>
+                    <LayoutBodyLeft onClick={this.onClickContent}>
+                        <Menu mobile={this.props.screenSize.mobile} open={this.menuIsOpen()} onClose={this.onCloseMenu}>
+                            {this.props.menuContent}
+                        </Menu>
+                    </LayoutBodyLeft>
+                    <LayoutBodyRight>
+                        {this.props.children}
+                    </LayoutBodyRight>
+                </LayoutBody>
+            </LayoutWrapper>
+        )
+    }
+})
