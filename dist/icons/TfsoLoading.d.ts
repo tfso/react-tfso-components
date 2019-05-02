3e55f4f073275ab9ead08adf2f00c095f9c159d8
tfsobob <appveyor@24sevenoffice.com>
2019-05-02 11:00:21 +0000

    Published by publish-to-git 457e85926c12124268ddcf7aa059da7c54f3f138 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Fix build error

---
+++ dist/icons/TfsoLoading.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SvgIconProps } from '@material-ui/core/SvgIcon';
+export declare type TfsoLoadingIconProps = {
+    /**
+     * @default infinite
+     */
+    once?: boolean;
+} & SvgIconProps;
+declare const _default: React.ComponentType<SvgIconProps>;
+export default _default;
+//# sourceMappingURL=TfsoLoading.d.ts.map
