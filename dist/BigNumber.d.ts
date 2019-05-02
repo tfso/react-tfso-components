d0c9399d29ef35d41beec000726e793aa52e695b
tfsobob <appveyor@24sevenoffice.com>
2019-05-02 10:09:08 +0000

    Published by publish-to-git 9c99bb5f34134b3cc0cf49b813fb5cb3e01a04a8 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Merge branch 'master' of https://github.com/tfso/react-tfso-components

---
+++ dist/BigNumber.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+export declare type BigNumberProps = {
+    /**
+     * Choose `light` when you have a dark background, otherwise `dark`
+     * @default light
+     */
+    color?: 'light' | 'dark';
+    /**
+     * @default large
+     */
+    size?: 'medium' | 'large';
+    children: string | number;
+};
+declare const _default: React.ComponentType<BigNumberProps>;
+export default _default;
+//# sourceMappingURL=BigNumber.d.ts.map
