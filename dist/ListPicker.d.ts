a63c748d801bb74a414319bbd7c9c82a72f2d9a6
tfsobob <appveyor@24sevenoffice.com>
2019-05-03 11:08:57 +0000

    Published by publish-to-git 34f7652e436dea4aed5d5244b80407b0db82c081 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Update home url to point to dashboard

---
+++ dist/ListPicker.d.ts
@@ -0,0 +1,19 @@
+export declare type ListPickerProps = {
+    open: boolean;
+    onCancel: () => void;
+    onSelect: (value: string) => void;
+    children?: undefined;
+    options: {
+        value: string;
+        label: string;
+    }[];
+    cancelButtonText: string;
+    searchLabel?: string;
+    anchorEl?: null | HTMLElement;
+    disabled?: string;
+    selected?: string;
+    avatarColor?: string;
+};
+declare const _default: any;
+export default _default;
+//# sourceMappingURL=ListPicker.d.ts.map
