d5eef2393dd83006d7730b5915727b45c83099d6
tfsobob <appveyor@24sevenoffice.com>
2019-05-06 14:25:22 +0000

    Published by publish-to-git 7127691432cb91dadff10e2ef8a106f1d6d1ed14 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Tsconfig fixes

---
+++ dist/ScreenSize.d.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import { WithWidth } from '@material-ui/core/withWidth';
+declare type Omit<T, K extends string> = Pick<T, Exclude<keyof T, K>>;
+export declare type ScreenSizeData = {
+    mobile: boolean;
+    tablet: boolean;
+    desktop: boolean;
+};
+export declare type InjectedScreenSizeProps = {
+    screenSize: ScreenSizeData;
+};
+export declare type ScreenSizeProps = {
+    children: (screenSize: ScreenSizeData) => any;
+} & WithWidth;
+declare const ScreenSize: React.ComponentType<Pick<ScreenSizeProps, "children"> & import("@material-ui/core/withWidth").WithWidthProps>;
+export declare function withScreenSize<P extends InjectedScreenSizeProps>(Component: React.ComponentType<P>): React.ComponentType<Omit<P, keyof InjectedScreenSizeProps>>;
+export declare const useScreenSize: () => never;
+export default ScreenSize;
+//# sourceMappingURL=ScreenSize.d.ts.map
