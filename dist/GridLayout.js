16de8c2ae3ccc078d823a5824b9739a1eaaa8f36
tfsobob <appveyor@24sevenoffice.com>
2019-05-06 14:09:55 +0000

    Published by publish-to-git f22933da2189f7c971d6fb5f6d252a6c47307327 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Fix icon return type

---
+++ dist/GridLayout.js
@@ -0,0 +1,224 @@
+import _styled from "styled-components";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+import React from 'react';
+import isEqual from 'lodash/isEqual';
+import Paper from '@material-ui/core/Paper';
+import RGL, { WidthProvider } from 'react-grid-layout/';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+var ReactGridLayout = WidthProvider(RGL);
+
+var BackgroundPaper = function BackgroundPaper(_ref) {
+  var backgroundColor = _ref.backgroundColor,
+      props = _objectWithoutProperties(_ref, ["backgroundColor"]);
+
+  return React.createElement(Paper, props);
+};
+
+var GridItemContainer = _styled(BackgroundPaper).withConfig({
+  displayName: "GridItemContainer",
+  componentId: "pvve1h-0"
+})(["&&{background-color:", ";width:100%;height:100%;}"], function (_ref2) {
+  var backgroundColor = _ref2.backgroundColor;
+  return backgroundColor;
+});
+
+var gridItemWidthNumberMap = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  11: 11,
+  12: 12,
+  'onequarter': 3,
+  'onethird': 4,
+  'half': 6,
+  'twothirds': 8,
+  'threequarters': 9,
+  'full': 12
+};
+var numberGridItemWithMap = {
+  1: 1,
+  2: 2,
+  3: 'onequarter',
+  4: 'onethird',
+  5: 5,
+  6: 'half',
+  7: 7,
+  8: 'twothirds',
+  9: 'threequarters',
+  10: 10,
+  11: 11,
+  12: 'full'
+};
+
+var toGridItemWidth = function toGridItemWidth(w) {
+  return w === undefined ? undefined : numberGridItemWithMap[Math.min(6, Math.trunc(w))];
+};
+
+var toGridItemHeight = function toGridItemHeight(w) {
+  return w === undefined ? undefined : Math.min(12, Math.trunc(w));
+};
+
+var gridItemPositionToLayout = function gridItemPositionToLayout(position) {
+  return {
+    i: position.id,
+    x: position.col,
+    y: position.row,
+    h: position.height,
+    w: gridItemWidthNumberMap[position.width],
+    minH: position.minHeight,
+    minW: position.minWidth && (gridItemWidthNumberMap[position.minWidth] || undefined),
+    maxH: position.maxHeight,
+    maxW: position.maxWidth && (gridItemWidthNumberMap[position.maxWidth] || undefined),
+    isDraggable: position.draggable !== undefined ? position.draggable : false,
+    isResizable: position.resizable !== undefined ? position.resizable : false,
+    static: position.static
+  };
+};
+
+var layoutToGridItemPosition = function layoutToGridItemPosition(layout) {
+  return {
+    id: layout.i,
+    col: layout.x,
+    row: layout.y,
+    width: toGridItemWidth(layout.w),
+    height: toGridItemHeight(layout.h),
+    minHeight: toGridItemHeight(layout.minH),
+    minWidth: toGridItemWidth(layout.minW),
+    maxHeight: toGridItemHeight(layout.maxH),
+    maxWidth: toGridItemWidth(layout.maxW),
+    draggable: layout.isDraggable,
+    resizable: layout.isResizable,
+    static: layout.static
+  };
+};
+
+var GridLayout =
+/*#__PURE__*/
+function (_React$PureComponent) {
+  _inherits(GridLayout, _React$PureComponent);
+
+  function GridLayout() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, GridLayout);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(GridLayout)).call.apply(_getPrototypeOf2, [this].concat(args)));
+    _this._layoutWasChangedWorkaround = false;
+
+    _this.onLayoutChange = function (layouts) {
+      var onLayoutChange = _this.props.onLayoutChange;
+      onLayoutChange && _this._layoutWasChangedWorkaround && onLayoutChange(layouts.map(layoutToGridItemPosition));
+      _this._layoutWasChangedWorkaround = false;
+    };
+
+    _this.validateItemLayout = function () {
+      if (process.env.NODE_ENV === 'production') return;
+      var items = new Set(_this.props.items.map(function (item) {
+        return item.id;
+      }));
+      var layouts = new Set(_this.props.layout.map(function (layout) {
+        return layout.id;
+      }));
+      items.forEach(function (item) {
+        if (!layouts.has(item)) {
+          console.error("Error: Item with id: ".concat(item, " is missing layout"));
+        }
+      });
+      layouts.forEach(function (layout) {
+        if (!items.has(layout)) {
+          console.warn("Warning: Layout with id: ".concat(layout, " does not correspond to any item"));
+        }
+      });
+    };
+
+    _this.onDragResizeStop = function (layout, oldItem, newItem) {
+      _this._layoutWasChangedWorkaround = !isEqual(oldItem, newItem);
+    };
+
+    return _this;
+  }
+
+  _createClass(GridLayout, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.validateItemLayout();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          items = _this$props.items,
+          layout = _this$props.layout,
+          _this$props$margin = _this$props.margin,
+          margin = _this$props$margin === void 0 ? 16 : _this$props$margin;
+      var rglLayout = Object.values(layout).map(gridItemPositionToLayout);
+      return React.createElement(ReactGridLayout, {
+        className: "layout",
+        cols: 12,
+        rowHeight: 90,
+        layout: rglLayout,
+        compactType: 'vertical',
+        margin: [margin, margin],
+        containerPadding: [0, 0],
+        autoSize: true,
+        measureBeforeMount: true // width= // TODO: set this one such that width is fixed, should be someting large, but not too large... hmm.
+        ,
+        useCSSTransforms: true,
+        preventCollision: false,
+        onLayoutChange: this.onLayoutChange,
+        onResizeStop: this.onDragResizeStop,
+        onDragStop: this.onDragResizeStop
+      }, items.map(function (_ref3) {
+        var id = _ref3.id,
+            backgroundColor = _ref3.backgroundColor,
+            children = _ref3.children;
+        return React.createElement(GridItemContainer, {
+          key: id,
+          backgroundColor: backgroundColor || 'inherit'
+        }, children);
+      }));
+    }
+  }]);
+
+  return GridLayout;
+}(React.PureComponent);
+
+GridLayout.defaultProps = {
+  margin: 16
+};
+export { GridLayout as default };
+//# sourceMappingURL=GridLayout.js.map
---
+++ dist/layout/Notify.d.ts
@@ -0,0 +1,124 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ListItemProps } from '@material-ui/core/ListItem';
+import { SvgIconProps } from '@material-ui/core/SvgIcon';
+export declare type NotificationItemProps = {
+    /**
+     * Unique identifier of the Notification.
+     * Spread to the `ListItem` component.
+     */
+    id: string;
+    /**
+     * Date when the notification was created.
+     */
+    date: Date;
+    /**
+     * `true` when the notification has been displayed to the user.
+     */
+    seen?: boolean;
+    /**
+     * `true` when the user has marked the notification as read either by:
+     * - marking all notifications as read.
+     * - clicking on the notification.
+     */
+    read?: boolean;
+    /**
+     * Callback when the user clicks the notification.
+     * you should set `read` to `true` in this callback before anything else.
+     */
+    onClick: () => void;
+    /**
+     * Should be either an `<Avatar>`, `<SvgIcon>` or `undefined`. Wrapped in a ListItemAvatar
+     */
+    avatar?: React.ReactElement;
+    /**
+     * <Typography> formatted text, or string
+     */
+    children: React.ReactChild;
+    action?: React.ReactChild;
+    ContainerComponent?: ListItemProps['ContainerComponent'];
+};
+declare const NotificationItem: {
+    (props: NotificationItemProps): JSX.Element;
+    propTypes: {
+        id: PropTypes.Validator<string>;
+        date: PropTypes.Validator<object>;
+        seen: PropTypes.Requireable<boolean>;
+        read: PropTypes.Requireable<boolean>;
+        onClick: PropTypes.Validator<(...args: any[]) => any>;
+        avatar: PropTypes.Requireable<PropTypes.ReactNodeLike>;
+        children: PropTypes.Validator<string | number | boolean | {} | PropTypes.ReactElementLike | PropTypes.ReactNodeArray>;
+        action: PropTypes.Requireable<PropTypes.ReactNodeLike>;
+        ContainerComponent: PropTypes.Requireable<any>;
+    };
+};
+export { NotificationItem };
+export declare type NotifierProps = {
+    /**
+     * Number of new notifications
+     */
+    count: number;
+    /**
+     * Display a loading indicator
+     */
+    loading?: boolean;
+    open: boolean;
+    /**
+     * Provide your translated value of e.g `Mark all as read`
+     */
+    readAllButtonText: string;
+    /**
+     * Callback when the user clicked the bell and opens the list of notifications.
+     * You may want to fetch new notifications here.
+     * When you're done fetching data, it is your responsibility to mark all notifications as read if the notifications is still open.
+     */
+    onOpen: () => void;
+    /**
+     * Invoked when the user clicks the `Mark all as read` button
+     * You should set the notifications as read here.
+     */
+    onReadAll: () => void;
+    /**
+     * Invoked when the user scrolled to the end of the list
+     */
+    onLoadMore: () => Promise<void>;
+    /**
+     * Invoked when the list is closed.
+     */
+    onClose: () => void;
+    /**
+     * Render your notifications here.
+     * The children will be wrapped in a `List`component.
+     * Typically a `NotificationItem`, but anything goes.
+     */
+    children: React.ReactNode;
+    /**
+     * Properties for the Notification Bell icon
+     */
+    IconProps?: SvgIconProps;
+};
+export default class Notifier extends React.PureComponent<NotifierProps> {
+    static propTypes: {
+        count: PropTypes.Validator<number>;
+        loading: PropTypes.Requireable<boolean>;
+        open: PropTypes.Validator<boolean>;
+        readAllButtonText: PropTypes.Validator<string>;
+        onOpen: PropTypes.Validator<(...args: any[]) => any>;
+        onReadAll: PropTypes.Validator<(...args: any[]) => any>;
+        onLoadMore: PropTypes.Validator<(...args: any[]) => any>;
+        onClose: PropTypes.Validator<(...args: any[]) => any>;
+        children: PropTypes.Validator<string | number | boolean | {} | PropTypes.ReactElementLike | PropTypes.ReactNodeArray>;
+        IconProps: PropTypes.Requireable<object>;
+    };
+    _anchorEl: React.RefObject<HTMLButtonElement>;
+    onOpen: () => void;
+    onClose: () => void;
+    renderNotifyer: () => JSX.Element;
+    renderToolbar: (mobile: boolean) => JSX.Element;
+    renderContent(mobile: boolean): JSX.Element;
+    renderDesktop(): JSX.Element;
+    renderMobile(): JSX.Element;
+    renderLoading: () => JSX.Element;
+    render(): JSX.Element;
+}
+//# sourceMappingURL=Notify.d.ts.map
