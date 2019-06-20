0fffd230fbc98812166128686d232b9a60970dba
tfsobob <appveyor@24sevenoffice.com>
2019-06-20 09:51:43 +0000

    Updates

--- precache-manifest.574e5075f428dcb07991bf885c869151.js
+++ precache-manifest.574e5075f428dcb07991bf885c869151.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "490f113911549e9360e7027c20b2b97c",
+    "revision": "9fd7196e585860d76b51b0d237a5cb9d",
     "url": "/react-tfso-components/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-tfso-components/static/css/2.0e027d9c.chunk.css"
   },
   {
-    "revision": "3f17c95f8494ece15969",
+    "revision": "6dc1d59ef700d9571536",
     "url": "/react-tfso-components/static/css/main.60b458d2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-tfso-components/static/js/2.1082df23.chunk.js"
   },
   {
-    "revision": "3f17c95f8494ece15969",
-    "url": "/react-tfso-components/static/js/main.44d6a929.chunk.js"
+    "revision": "6dc1d59ef700d9571536",
+    "url": "/react-tfso-components/static/js/main.fe1cc724.chunk.js"
   },
   {
     "revision": "2c5a8edee8ab9a7980cc",
