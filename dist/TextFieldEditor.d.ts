6724b50e0d9aab5319eba8c957ac260afd59d4ef
tfsobob <appveyor@24sevenoffice.com>
2019-07-02 13:11:05 +0000

    Published by publish-to-git 915ddbca36d05a5a7f00f546c41797b213037044 (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) translate prop for notify

---
+++ dist/TextFieldEditor.d.ts
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextFieldProps } from '@material-ui/core/TextField';
+declare type Props = {
+    value: string;
+    onChange: (value: string) => void;
+    dirty?: boolean;
+    enableDirtyCheck?: boolean;
+};
+declare type State = {
+    editing: boolean;
+    value: string;
+    prevProps: Props;
+};
+declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+export declare type TextFieldEditorProps = Props & Partial<Omit<TextFieldProps, 'value' | 'onChange'>>;
+export default class TextFieldEditor extends React.PureComponent<TextFieldEditorProps, State> {
+    static propTypes: {
+        value: PropTypes.Validator<string>;
+        onChange: PropTypes.Validator<(...args: any[]) => any>;
+        dirty: PropTypes.Requireable<boolean>;
+    };
+    _inputRef: React.RefObject<HTMLInputElement>;
+    state: State;
+    static getDerivedStateFromProps(props: Readonly<TextFieldEditorProps>, state: Readonly<State>): {
+        value: string;
+        prevProps: Readonly<TextFieldEditorProps>;
+    } | {
+        prevProps: Readonly<TextFieldEditorProps>;
+        value?: undefined;
+    };
+    blur: () => void;
+    onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+    onFocus: (e: (event: any) => void) => void;
+    onBlur: (e: (event: any) => void) => void;
+    onChange: (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void;
+    render(): JSX.Element;
+}
+export {};
+//# sourceMappingURL=TextFieldEditor.d.ts.map
---
+++ dist/board/BoardItemPlaceholder.js
@@ -0,0 +1,22 @@
+import _styled from "styled-components";
+
+var BoardItemPlaceholder = _styled.div.attrs(function (props) {
+  return {
+    style: {
+      // This property changes very often, if it's in the styled macro, a new class will be generated for each change to the position.
+      transform: "translate(".concat(props.left, "px, ").concat(props.top, "px)")
+    }
+  };
+}).withConfig({
+  displayName: "BoardItemPlaceholder",
+  componentId: "sc-79qbm1-0"
+})(["&&{position:absolute;width:", "px;height:", "px;background:red;opacity:0.2;transition:transform 100ms ease;z-index:2;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;}"], function (_ref) {
+  var width = _ref.width;
+  return width;
+}, function (_ref2) {
+  var height = _ref2.height;
+  return height;
+});
+
+export default BoardItemPlaceholder;
+//# sourceMappingURL=BoardItemPlaceholder.js.map
