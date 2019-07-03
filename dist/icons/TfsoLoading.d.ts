d53cc5a7a24944233e4f79a110d1a53d4f277ddc
tfsobob <appveyor@24sevenoffice.com>
2019-07-03 07:01:16 +0000

    Published by publish-to-git 1c2299ea1e7010e9f22cc6752f400746475756ae (HEAD, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) changed translation and updated component

---
+++ dist/icons/TfsoLoading.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SvgIconProps } from '@material-ui/core/SvgIcon';
+export declare type TfsoLoadingIconProps = {
+    /**
+     * @default infinite
+     */
+    once?: boolean;
+} & SvgIconProps;
+declare const TfsoLoading: React.MemoExoticComponent<(props: TfsoLoadingIconProps) => JSX.Element>;
+export default TfsoLoading;
+//# sourceMappingURL=TfsoLoading.d.ts.map
